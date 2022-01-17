5201dcca47a6a00fdb90e8a72df98daa8fb2d2d9
Dyrhoi <realarcoz@gmail.com>
2022-01-17 22:55:55 +0100

    Fix cloudinary provider for strapi 4.0

--- config/plugins.js
+++ config/plugins.js
@@ -1,11 +1,21 @@
+//path: ./config/plugins.js
+
 module.exports = ({ env }) => ({
-    // ...
-    upload: {
+  // ...
+  upload: {
+    config: {
       provider: 'cloudinary',
       providerOptions: {
         cloud_name: env('CLOUDINARY_NAME'),
         api_key: env('CLOUDINARY_KEY'),
         api_secret: env('CLOUDINARY_SECRET'),
       },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
     },
-  });
+  },
+  // ...
+});
+ 
